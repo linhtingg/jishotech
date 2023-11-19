f640ef41ba1f91980b7d7aa88d46fe77cc40681e
Phạm Vân Anh <85979283+Vananh0110@users.noreply.github.com>
2023-11-19 16:13:54 +0700

    Fix css for header, footer, nav, search page

--- user/includes/header.php
+++ user/includes/header.php
@@ -6,8 +6,7 @@
     </div>
 
     <div class="header-navbar">
-      <nav class="navbar-custom navbar ">
-          <ul class="nav navbar-nav">
+          <ul class="nav nav-pills navbar-custom">
             <li class="nav-item active">
               <a href="#" class="nav-link text-center">単語の探索</a>
             </li>
@@ -21,29 +20,29 @@
               <a href="#" class="nav-link text-center">アクション歴史</a>
             </li>
           </ul>
-        </nav>
     </div>
   </div>
       
   <div class="header-right">
-  <div class="dropdown">
-    <button class="btn-dd" type="button" data-toggle="dropdown">ユーザー名
-    <span class="caret"></span></button>
-    <ul class="dropdown-menu">
-      <li>
-        <a href="#">
-          <img src="./images/header/avatar-icon.png" alt="avatar-icon">
-          プロフィール
-        </a>
-      </li>
-      <li>
-        <a href="#">
-          <img src="./images/header/signout-icon.png" alt="signout-icon">
-          サインアウト
-        </a>
-      </li>
-    </ul>
-  </div>
+    <div class="dropdown">
+      <button class="btn-dd dropdown-toggle" type="button" data-bs-toggle="dropdown">
+        ユーザー名
+      </button>
+      <ul class="dropdown-menu">
+        <li>
+          <a class="dropdown-item" href="#">
+            <img src="./images/header/avatar-icon.png" alt="avatar-icon">
+            プロフィール
+          </a>
+        </li>
+        <li>
+          <a class="dropdown-item" href="#">
+            <img src="./images/header/signout-icon.png" alt="signout-icon">
+            サインアウト
+          </a>
+        </li>
+      </ul>
+    </div>
   </div>
 </header>
 
