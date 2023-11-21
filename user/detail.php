466987477fd383dc1a698640e4bd198d5d6447ac
Phạm Vân Anh <85979283+Vananh0110@users.noreply.github.com>
2023-11-21 09:25:10 +0700

    Detail page

---
+++ user/detail.php
@@ -0,0 +1,115 @@
+<?php
+include('includes/config.php');
+session_start();
+error_reporting(0);
+
+?>
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<title>JishoTech || Home Page</title>
+	<!-- <link href="css/bootstrap.css" rel='stylesheet' type='text/css' /> -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+	<!-- Custom Theme files -->
+	<link href="css/style.css" rel='stylesheet' type='text/css' />
+	<!--Custom Theme files-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+
+	<script
+		type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+	</script>
+	<script src="js/jquery-1.8.3.min.js"></script>
+	<script src="js/modernizr.custom.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+	
+
+	<!--start-smoth-scrolling-->
+	<script type="text/javascript" src="js/move-top.js"></script>
+	<script type="text/javascript" src="js/easing.js"></script>
+
+	<script type="text/javascript">
+		jQuery(document).ready(function ($) {
+			$(".scroll").click(function (event) {
+				event.preventDefault();
+				$('html,body').animate({ scrollTop: $(this.hash).offset().top }, 1000);
+			});
+		});
+	</script>
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+	
+	<!--start-smoth-scrolling-->
+	<!--webfonts-->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Kosugi+Maru&display=swap" rel="stylesheet">
+	<!--webfonts-->
+
+
+</head>
+
+<body>
+	<?php include_once('includes/header.php'); ?>
+	<div class="content">
+		<?php include_once('includes/search.php'); ?>
+		<div class="row">
+			<div class="col col-3">
+				<div class="card mb-4 detail-custom">
+                    <div class="card-body">
+                        <div class="card-title">登録</div>
+                        <div class="card-text">「とうろく」</div>
+                    </div>
+                </div>
+                <ul class="word-topic list-unstyled">
+                    <li class="text-center">情報セキュリティ</li>
+                    <li class="text-center">ネットワーク</li>
+                </ul>
+			</div>
+			<div class="col col-8">
+				<div class="detail-content">
+					
+                    <div class="detail-function d-flex justify-content-end">
+                        <button type="button" class="detail-edit">
+                            <i class="bi bi-pencil-square"></i>
+                        </button>
+                        <button type="button" class="detail-bookmark">
+                            <i class="bi bi-bookmark"></i>
+                        </button>
+                    </div>
+
+					<div class="word-mean" style="min-height: 150px">
+						<div class="word-vn">
+							Đăng ký
+						</div>
+					</div>
+
+					<div class="d-flex mt-4 justify-content-between">
+						<div class="detail-img">
+							<img src="./images/img1.jpg" alt="" style="height: auto; width: 200px; margin-right: 20px;">
+							<img src="./images/img2.png" alt="" style="height: auto; width: 200px;">
+							
+						</div>
+					<div class="img-btn d-flex justify-content-center align-items-center">
+						<button type="button">語彙の他の画像</button> 
+					</div>
+						
+
+					</div>
+					
+		
+                </div>
+			</div>
+		</div>
+		
+	</div>
+
+
+	<?php include_once('includes/footer.php'); ?>
+
+
+</body>
+
+</html>
--- user/topic.php
+++ user/topic.php
@@ -52,9 +52,8 @@ $topics = $topicQuery->fetch_all(MYSQLI_ASSOC);
     });
     </script>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
